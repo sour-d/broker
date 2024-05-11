feaac9543bd1d6a008c7d97978579ecde6e4a66a
sourav das <36912347+sour-d@users.noreply.github.com>
2024-05-11 16:41:01 +0530

    fixed fetch logic

--- src/HistoricalKline.js
+++ src/HistoricalKline.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import fs from "fs";
 import _ from "lodash";
+import dayjs from "dayjs";
 
 const restClient = new RestClientV5({
   key: process.env.TESTNET_API_KEY,
@@ -12,34 +13,53 @@ const restClient = new RestClientV5({
   // demoTrading: true,
 });
 
+const getNewEnd = (start, end, addOneSecond = false) => {
+  let newStart = start;
+  if (addOneSecond) {
+    newStart = dayjs(start).add(1, "second").valueOf();
+  }
+  const newEnd = dayjs(newStart)
+    .add(10, "hour")
+    .subtract(1, "second")
+    .valueOf();
+  if (newEnd > end) {
+    return end;
+  }
+  return newEnd;
+};
+
+const formatResponse = (response) => {
+  return response.result.list
+    .map((kline) => {
+      return {
+        DateStr: new Date(+kline[0]),
+        Date: +kline[0],
+        Open: kline[1],
+        High: kline[2],
+        Low: kline[3],
+        Close: kline[4],
+        Volume: kline[5],
+      };
+    })
+    .reverse();
+};
+
 const HistoricalKline = async (symbol, interval, start, end) => {
   let allData = [];
-  let lastIntervalFetched = end;
+  let fetchTill = getNewEnd(start, end);
 
-  while (lastIntervalFetched !== start) {
-    console.log({ lastIntervalFetched, start, end });
+  while (end >= fetchTill) {
     await restClient
-      .getKline({ symbol, interval, start, end: lastIntervalFetched })
+      .getKline({ symbol, interval, start, end: fetchTill, limit: 1000 })
       .then((response) => {
-        const data = response.result.list.map((kline) => {
-          return {
-            Date: kline[0],
-            Open: kline[1],
-            High: kline[2],
-            Low: kline[3],
-            Close: kline[4],
-            Volume: kline[5],
-          };
-        });
-        if (_.first(allData)?.Date === _.first(data).Date) {
-          console.log("first data received is same as last data fetched");
-          lastIntervalFetched = start;
+        const data = formatResponse(response);
+        allData = allData.concat(data);
+        start = fetchTill;
+        fetchTill = getNewEnd(start, end, true);
+        if (fetchTill === start) {
+          fetchTill += 1;
           return;
         }
-        console.log("last data received", _.first(data));
-        allData = data.concat(allData);
-        lastIntervalFetched = _.last(data).Date;
-        console.log(lastIntervalFetched);
       })
       .catch((error) => {
         throw error;
@@ -51,3 +71,11 @@ const HistoricalKline = async (symbol, interval, start, end) => {
 };
 
 export default HistoricalKline;
+
+// const startMs = new Date("2024-05-09T00:00:00.000Z").getTime();
+// const endMs = new Date("2024-05-09T23:59:59.999Z").getTime();
+
+// console.log(startMs, endMs);
+
+// const data = await HistoricalKline("BTCUSD", "1", startMs, endMs);
+// fs.writeFileSync("data.json", JSON.stringify(data));
