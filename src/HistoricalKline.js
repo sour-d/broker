f3f1f23495069a93c7fa37ba74619903ff1b7737
sourav das <36912347+sour-d@users.noreply.github.com>
2024-05-12 12:08:47 +0530

    fixed dayjs parsing issue

--- src/HistoricalKline.js
+++ src/HistoricalKline.js
@@ -1,9 +1,14 @@
 import { RestClientV5 } from "bybit-api";
 import dotenv from "dotenv";
-dotenv.config();
 import fs from "fs";
 import _ from "lodash";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
+
+dotenv.config();
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const restClient = new RestClientV5({
   key: process.env.TESTNET_API_KEY,
